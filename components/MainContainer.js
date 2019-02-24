3a2ba97bdbe8daeaea22f92bad76d342d5a27c08
cdsleaf <cdsleaf@gmail.com>
2019-02-24 17:21:37 +1300

    refactoring: extract main container (navigation module) from app.js

---
+++ components/MainContainer.js
@@ -0,0 +1,85 @@
+import { Platform } from 'react-native';
+import { 
+  createAppContainer,
+  createBottomTabNavigator,
+  createMaterialTopTabNavigator,
+  createStackNavigator,   
+} from 'react-navigation';
+import { FontAwesome, Ionicons } from '@expo/vector-icons'; 
+import { purple, white } from './utils/colors';
+import Decks from './components/Decks';
+import AddDeck from './components/AddDeck';
+import DeckMain from './components/DeckMain';
+import AddCard from './components/AddCard';
+
+const RouteConfigs = {
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddDecks: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Decks',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+};
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    }
+  }
+}
+
+const Tabs = Platform.OS === "ios" 
+  ? createBottomTabNavigator(RouteConfigs, TabNavigatorConfig)
+  : createMaterialTopTabNavigator(RouteConfigs, TabNavigatorConfig);
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null,
+    }
+  },
+  DeckMain: {
+    screen: DeckMain,
+    navigationOptions: ({ navigation }) => ({
+      title: navigation.state.params.title,
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    })
+  },
+  AddCard: {
+    screen: AddCard,
+    navigationOptions: {
+      title: 'Add Card',
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple,
+      }
+    }
+  }
+})
+
+export default createAppContainer(MainNavigator);
